6e98ed5ed9b25b7418409a465b66b5fcbab1cf68
jhamel <jhamel@9c9986f9-cfd4-0310-ab54-982a9523a5c0>
2006-07-13 13:23:28 +0000

    M    ProgramersInfo/src2dia.py

--- ProgramersInfo/src2dia.py
+++ ProgramersInfo/src2dia.py
@@ -126,17 +126,20 @@ class Footer:
 class startObject:
     def __init__(self):
         self.classname = None
-
+        self.X1 = 10.0
+        self.Y1 = 20.0
+        self.color = '#ffffff'
+        self.id = '00'
     def getXml(self):    
-        s = '           <dia:object type="UML - Class" version="0" id="O0">  \n \
+        s = '           <dia:object type="UML - Class" version="0" id="%s">  \n \
               <dia:attribute name="obj_pos">  \n \
-                <dia:point val="50.0,50.0"/>  \n \
+                <dia:point val="%f,%f"/>  \n \
               </dia:attribute>  \n \
               <dia:attribute name="obj_bb">  \n \
                 <dia:rectangle val="50.0,50.0;50.0,50.0"/>  \n \
               </dia:attribute>  \n \
               <dia:attribute name="elem_corner">  \n \
-                <dia:point val="40.0,40.0"/>  \n \
+                <dia:point val="%f,%f"/>  \n \
               </dia:attribute>  \n \
               <dia:attribute name="elem_width">  \n \
                 <dia:real val="1.0"/>  \n \
@@ -169,7 +172,7 @@ class startObject:
                 <dia:color val="#000000"/>  \n \
               </dia:attribute>  \n \
               <dia:attribute name="background_color">  \n \
-                <dia:color val="#ffffff"/>  \n \
+                <dia:color val="%s"/>  \n \
               </dia:attribute>  \n \
               <dia:attribute name="normal_font">  \n \
                 <dia:font name="Courier"/>  \n \
@@ -197,7 +200,7 @@ class startObject:
               </dia:attribute>  \n \
               <dia:attribute name="template">  \n \
                 <dia:boolean val="false"/>  \n \
-              </dia:attribute>  \n ' %self.classname
+              </dia:attribute>  \n ' %( self.id, self.X1, self.Y1,self.X1, self.Y1, self.classname,self.color)
         return s
         
 class endObject:
@@ -292,7 +295,82 @@ class endParameters:
     def getXml(self):         
         s = '</dia:attribute> \n '
         return s
-              
+            
+class umlInherits:
+    def __init__(self):
+        self.name = 'test'
+        self.X = 40.0
+        self.Y = 60.0
+        self.targetX = 70.0
+        self.targetY = 100.0
+        
+        self.P1x = 10.0
+        self.P1y = 10.0
+        self.P2x = 20.0
+        self.P2y = 10.0
+        self.P3x = 30.0
+        self.P3y = 10.0
+        self.P4x = 40.0
+        self.P4y = 10.0
+        self.ConnID1 = '0'
+        self.ConnID2 = '1'
+        
+        self.Conn1 = '0'
+        self.Conn2 = '1'
+    def setValues(self):
+        self.P1x = self.X
+        self.P1y = self.Y
+        self.P2x = self.X 
+        self.P2y = self.Y - 10
+        self.P3x = self.targetX 
+        self.P3y = self.targetY - 10
+        self.P4x = self.targetX
+        self.P4y = self.targetY
+        
+    def getXml(self):         
+        s =  ' <dia:object type="UML - Generalization" version="1" id="O65">  \n \
+              <dia:attribute name="obj_pos"> \n \
+                <dia:point val="%f,%f"/> \n \
+              </dia:attribute> \n \
+              <dia:attribute name="obj_bb"> \n \
+                <dia:rectangle val="0,0;0,0"/> \n \
+              </dia:attribute> \n \
+              <dia:attribute name="orth_points"> \n \
+                <dia:point val="%f,%f"/> \n \
+                <dia:point val="%f,%f"/> \n \
+                <dia:point val="%f,%f"/> \n \
+                <dia:point val="%f,%f"/> \n \
+              </dia:attribute> \n \
+              <dia:attribute name="orth_orient"> \n \
+                <dia:enum val="1"/> \n \
+                <dia:enum val="0"/> \n \
+                <dia:enum val="1"/> \n \
+              </dia:attribute> \n \
+              <dia:attribute name="orth_autoroute"> \n \
+                <dia:boolean val="true"/> \n \
+              </dia:attribute> \n \
+              <dia:attribute name="text_colour"> \n \
+                <dia:color val="#000000"/> \n \
+              </dia:attribute> \n \
+              <dia:attribute name="line_colour"> \n \
+                <dia:color val="#000000"/> \n \
+              </dia:attribute> \n \
+              <dia:attribute name="name"> \n \
+                <dia:string>##</dia:string> \n \
+              </dia:attribute> \n \
+              <dia:attribute name="stereotype"> \n \
+                <dia:string>##</dia:string> \n \
+              </dia:attribute> \n \
+              <dia:connections> \n \
+                <dia:connection handle="0" to="%s" connection="%s"/> \n \
+                <dia:connection handle="1" to="%s" connection="%s"/> \n \
+              </dia:connections> \n \
+            </dia:object>          \n ' \
+            %( self.X, self.Y, self.P1x, self.P1y, self.P2x, self.P2y, self.P3x, self.P3y, self.P4x, self.P4y, self.ConnID1, self.ConnID2, self.Conn1, self.Conn2)
+            
+        return s
+        
+        
 def colorstr(rgb): 
     return "#%x%x%x" % (rgb[0]/16,rgb[1]/16,rgb[2]/16)
 
@@ -301,6 +379,8 @@ def getParameter(dia, m,sLine):
     print m
     print sLine, m.end()
     print sLine[m.end():]
+    liPara = None
+    opname = None
     m2 = dia.rBracketsOn.match(sLine)
     if m2:
         print 'm2', sLine[m.end():m2.end()-1]
@@ -314,72 +394,175 @@ def getParameter(dia, m,sLine):
             
             print 'm3', sLine[m2.end():m3.end()-1]
             liPara = sLine[m2.end():m3.end()-1]
-    print 'opname', opname
-    liPara = liPara.split(',')
-    for i in range(len(liPara)):
-        liPara[i] = liPara[i].strip()
-    print 'liPara', liPara
+        print 'opname', opname
+    else:
+        opname = sLine[m.end():len(sLine)-1]
+    if liPara:    
+        liPara = liPara.split(',')
+        for i in range(len(liPara)):
+            liPara[i] = liPara[i].strip()
+        print 'liPara', liPara
     print '****************************************'
     
     
     return opname,liPara
         
 def start():
-    filenames = ['./CUON/Cuon.py']
-    
+    seriesX = [] 
+    counterHex1 = 0x0
+    liS = []
+    f = open('files.txt','r')
+    s = f.readline()
+    while s:
+        s = s.strip()
+        if len(s) > 0 and s[0] != '#':
+            
+            liS = s.split(',')
+            if len(liS) == 1:
+                liS.append(`counterHex1`)
+            if len(liS) == 2:                    
+                liS.append('#BADD99')
+            if len(liS) == 3:                    
+                liS.append('0x0')
+            
+            seriesX.append(deepcopy(liS))
+
+        s = f.readline()
+        counterHex1 += 1
+        
+    print seriesX
+        
     
     dia = src2dia('cuon')
-    
+    print seriesX
     dia.add(Header())
- 
-    for fName in filenames:
-        f = open(fName,'r')
+    
+    counterX1 = 0
+    counterY1 = -1
+    dicClasses = {}
+    liClasses = []
+
+    for fName in seriesX:
+        counterX1 += 1
+        filename = fName[0]
+        sID = fName[1]
+        sColor = fName[2]
+        sXY = fName[3]
+        
+        f = open(filename,'r')
         sLine = f.readline()
+        
         sO = startObject()
         sD = startUMLOperation()
         sP = parameters()
         while sLine:
             sLine = sLine.strip()
-            m = dia.rClass.match(sLine)
-            if m:
-                sO.classname, liInherits = getParameter(dia, m,sLine)
-                print '#####################'
-                print sO.classname
-                print liInherits
-                dia.add(deepcopy(sO))
-                dia.add(startOperations())
-            m = dia.rDef.match(sLine)
-            if m:
-                print m.end()
-                print sLine
-                s1,li1 = getParameter(dia, m,sLine)
-                print '++++++++++++++++++++++++++'
-                print s1
-                print li1
-                sD.defname = s1
-                dia.add(deepcopy(sD))
-                dia.add(startParameters())
-                for i in li1:
-                    print 'paraname', i
-                    sP.paraname = i
-                    dia.add(deepcopy(sP))
-                dia.add(endParameters())
-                dia.add(endUMLOperation())
+            if len(sLine) > 0 and sLine[0] == '#':
+                pass
+            else:
                 
+            
+                m = dia.rClass.match(sLine)
+                if m:
+                    sO.classname, liInherits = getParameter(dia, m,sLine)
+                    dicClasses['Name'] = sO.classname
+                    dicClasses['inherits'] = liInherits
+                    
+                    sO.color = sColor
+                    if sXY == '0x0':
+                        sO.X1 = float(counterX1 * 25 + 10)
+                        sO.Y1 = float(counterY1 *60 + 10)
+                    else:
+                        liXY = sXY.split('x')
+                        sO.X1 = float(liXY[0].strip())
+                        sO.Y1 = float(liXY[1].strip())
+                        
+                    sO.id = sID
+                    print '#####################'
+                    print sO.classname
+                    print liInherits
+                    dicClasses['Name'] = sO.classname
+                    dicClasses['X1'] = sO.X1
+                    dicClasses['Y1'] = sO.Y1
+                    dicClasses['id'] = sO.id
+                    liClasses.append(deepcopy(dicClasses))
+
+                    dia.add(deepcopy(sO))
+                    dia.add(startOperations())
+                m = dia.rDef.match(sLine)
+                if m:
+                    print m.end()
+                    print sLine
+                    s1,li1 = getParameter(dia, m,sLine)
+                    print '++++++++++++++++++++++++++'
+                    print s1
+                    print li1
+                    sD.defname = s1
+                    dia.add(deepcopy(sD))
+                    dia.add(startParameters())
+                    for i in li1:
+                        print 'paraname', i
+                        sP.paraname = i
+                        dia.add(deepcopy(sP))
+                    dia.add(endParameters())
+                    dia.add(endUMLOperation())
+                    
             sLine = f.readline()
-            
+                
     
-    
-#        dia.add(parameters())
-#        dia.add(parameters())
         
+    #        dia.add(parameters())
+    #        dia.add(parameters())
             
-        
-        
+                
+            
+            
         dia.add(endOperations())
     
     
         dia.add(endObject())
+
+
+    oInherits = umlInherits()
+        
+    print 'Classes', liClasses
+    liInherits = []  
+    counterHex2 = 0xA0    
+    for inh1 in liClasses:   
+        if inh1['inherits']:
+            for inherits in inh1['inherits']:
+                print '-------------------------'
+
+                print inh1['Name'], len(inherits)
+                print 'Inherits',  inherits
+            
+                for search1 in liClasses:
+                    if search1['Name'] == inherits:
+                        oInherits.X = search1['X1']
+                        oInherits.Y = search1['Y1']
+                        oInherits.connID1 = search1['id']
+                        print 'Search1-ID', search1['id']
+                        
+                oInherits.name = inh1['Name']
+                oInherits.targetX = inh1['X1']
+                oInherits.targetY = inh1['Y1']
+                oInherits.connID2 = inh1['id']
+                
+                oInherits.setValues()
+                
+                oInherits.Conn1 = `counterHex2`
+                oInherits.Conn2 = `counterHex2`
+                
+                counterHex2 += 1
+                
+    
+    
+    
+                liInherits.append(deepcopy(oInherits))
+    
+    for u1 in liInherits:
+        dia.add(u1)
+        
     dia.add(Footer())
     
     dia.write_dia()
