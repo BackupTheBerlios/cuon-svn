bc94ff2582521f63365181d42d4ad750dc24ea75
jhamel <jhamel@9c9986f9-cfd4-0310-ab54-982a9523a5c0>
2011-02-27 12:38:05 +0000

    bugs at order prizes solved

--- cuon_client/Cuon.py
+++ cuon_client/Cuon.py
@@ -301,7 +301,7 @@ class MainWindow(windows):
         
         windows.__init__(self)
         self.sStartType = sT
-        self.Version = {'Major': 11, 'Minor': 2, 'Rev': 23, 'Species': 0, 'Maschine': 'Linux,BSD,Windows,Mac'}
+        self.Version = {'Major': 11, 'Minor': 2, 'Rev': 27, 'Species': 0, 'Maschine': 'Linux,BSD,Windows,Mac'}
         
         self.sTitle =  `self.Version['Major']` + '.' + `self.Version['Minor']` + '.' + `self.Version['Rev']` 
         self.t0 = None
